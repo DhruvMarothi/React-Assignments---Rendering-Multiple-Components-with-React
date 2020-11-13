05b655fa6ae290f443ab904ecf3e34b22014f16c
DhruvMarothi <51503387+DhruvMarothi@users.noreply.github.com>
2020-11-13 22:47:59 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <div>
+      <h1>Edtech</h1>
+      <h3>
+        An online platform for schools to provide education virtually to the
+        children
+      </h3>
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-
-
 ReactDOM.render(<App />, document.getElementById("root"));
+
+let element = document.getElementsByTagName("h1");
+element.setAttribute(data - ns - test, "project-name");
+
+element = document.getElementsByTagName("h3");
+element.setAttribute(data - ns - test, "project-description");
