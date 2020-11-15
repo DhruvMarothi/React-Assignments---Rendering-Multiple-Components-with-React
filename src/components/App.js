687fa493b7c4b2be4845487e5b5fd740b6fc02a4
DhruvMarothi <51503387+DhruvMarothi@users.noreply.github.com>
2020-11-15 21:11:40 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import "../styles/App.css";
 const App = () => {
   return (
     <div>
-      <h1 data - ns - test = "project-name">Edtech</h1>
-      <h3 data - ns - test = "project-description">
+      <h1 data-ns-test="project-name">Edtech</h1>
+      <h3 data-ns-test="project-description">
         An online platform for schools to provide education virtually to the
         children
       </h3>
