e66a049f5818babbcabab6fe969ba12a5f96c116
DhruvMarothi <51503387+DhruvMarothi@users.noreply.github.com>
2020-11-15 21:06:08 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,3 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-let element = document.getElementsByTagName("h1");
-element.setAttribute(data - ns - test, "project-name");
-
-element = document.getElementsByTagName("h3");
-element.setAttribute(data - ns - test, "project-description");
